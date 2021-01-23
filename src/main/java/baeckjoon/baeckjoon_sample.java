c5493ab433d77b0e73e477fb0e59dfb8008dbed9
Hyeon9mak <jinha3507@gmail.com>
2021-01-23 15:08:14 +0900

    feat: add baekjoon problem solving sample code

---
+++ src/main/java/baeckjoon/baeckjoon_sample.java
@@ -0,0 +1,11 @@
+package baeckjoon;
+
+import java.util.Scanner;
+
+public class baeckjoon_sample {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
