2925e8e92602c0da9763c532339cab9f89744fb9
Hyeon9mak <jinha3507@gmail.com>
2021-01-29 14:53:54 +0900

    refactor: fix IO method for more faster

---
+++ src/main/java/baekjoon/b2869_달팽이는_올라가고_싶다.java
@@ -0,0 +1,2 @@
+package baekjoon;public class b2869_달팽이는_올라가고_싶다 {
+}
--- src/main/java/baekjoon/baeckjoon_sample.java
+++ src/main/java/baekjoon/baeckjoon_sample.java
@@ -1,11 +1,14 @@
 package baekjoon;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class baeckjoon_sample {
 
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
     }
 }
