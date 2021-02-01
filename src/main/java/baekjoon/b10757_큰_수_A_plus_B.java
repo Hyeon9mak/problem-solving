f6745ea9356d92003df1ed59e17ede8e91443684
Hyeon9mak <jinha3507@gmail.com>
2021-02-02 08:48:50 +0900

    problem-solving: baekjoon-10757

---
+++ src/main/java/baekjoon/b10757_큰_수_A_plus_B.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class b10757_큰_수_A_plus_B {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        System.out.println(A.add(B));
+    }
+
+}
+
+/*
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A,B < 1010000)
+
+출력
+첫째 줄에 A+B를 출력한다.
+
+예제 입력 1
+9223372036854775807 9223372036854775808
+예제 출력 1
+18446744073709551615
+ */
