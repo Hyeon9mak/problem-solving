44a3d23c3a9b9f6a6dd3c4e33531a3a6007e7580
Hyeon9mak <jinha3507@gmail.com>
2021-02-09 08:49:08 +0900

    problem-solving: baekjoon-1085

---
+++ src/main/java/baekjoon/b1085_직사각형에서_탈출.java
@@ -0,0 +1,46 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class b1085_직사각형에서_탈출 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        List<Integer> points = new ArrayList<>(Arrays.asList(x, y, w - x, h - y));
+        System.out.println(points.stream().min(Integer::compareTo).get());
+    }
+
+}
+
+/*
+문제
+한수는 지금 (x, y)에 있다. 직사각형의 왼쪽 아래 꼭짓점은 (0, 0)에 있고, 오른쪽 위 꼭짓점은 (w, h)에 있다. 직사각형의 경계선까지 가는 거리의 최솟값을 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 x, y, w, h가 주어진다.
+
+출력
+첫째 줄에 문제의 정답을 출력한다.
+
+제한
+1 ≤ w, h ≤ 1,000
+1 ≤ x ≤ w-1
+1 ≤ y ≤ h-1
+x, y, w, h는 정수
+예제 입력 1
+6 2 10 3
+예제 출력 1
+1
+ */
