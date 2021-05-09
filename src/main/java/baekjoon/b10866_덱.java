de1bfd0e75968ffb3dbe4fa6773c1de1d4f06ee8
hyeon9mak <jinha3507@gmail.com>
2021-05-10 00:31:55 +0900

    problem-solving: baekjoon-10866

---
+++ src/main/java/baekjoon/b10866_덱.java
@@ -0,0 +1,184 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class b10866_덱 {
+
+    private static final String PUSH_FRONT = "push_front";
+    private static final String PUSH_BACK = "push_back";
+    private static final String POP_FRONT = "pop_front";
+    private static final String POP_BACK = "pop_back";
+    private static final String SIZE = "size";
+    private static final String EMPTY = "empty";
+    private static final String FRONT = "front";
+    private static final String BACK = "back";
+    private static final String REGEX = " ";
+    private static final int NUMBER = 1;
+    private static final String ENTER_LINE = "\n";
+    private static final int TRUE = 1;
+    private static final int FALSE = 0;
+    private static final int ERROR = -1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int commandCount = Integer.parseInt(br.readLine());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < commandCount; i++) {
+            String command = br.readLine();
+
+            if (command.startsWith(PUSH_FRONT)) {
+                deque.addFirst(Integer.parseInt(command.split(REGEX)[NUMBER]));
+            }
+
+            if (command.startsWith(PUSH_BACK)) {
+                deque.addLast(Integer.parseInt(command.split(REGEX)[NUMBER]));
+            }
+
+            if (command.startsWith(POP_FRONT)) {
+                if (deque.isEmpty()) {
+                    stringBuilder.append(ERROR).append(ENTER_LINE);
+                } else {
+                    stringBuilder.append(deque.pollFirst()).append(ENTER_LINE);
+                }
+            }
+
+            if (command.startsWith(POP_BACK)) {
+                if (deque.isEmpty()) {
+                    stringBuilder.append(ERROR).append(ENTER_LINE);
+                } else {
+                    stringBuilder.append(deque.pollLast()).append(ENTER_LINE);
+                }
+            }
+
+            if (command.startsWith(SIZE)) {
+                stringBuilder.append(deque.size()).append(ENTER_LINE);
+            }
+
+            if (command.startsWith(EMPTY)) {
+                if (deque.isEmpty()) {
+                    stringBuilder.append(TRUE).append(ENTER_LINE);
+                } else {
+                    stringBuilder.append(FALSE).append(ENTER_LINE);
+                }
+            }
+
+            if (command.startsWith(FRONT)) {
+                if (deque.isEmpty()) {
+                    stringBuilder.append(ERROR).append(ENTER_LINE);
+                } else {
+                    stringBuilder.append(deque.peekFirst()).append(ENTER_LINE);
+                }
+            }
+
+            if (command.startsWith(BACK)) {
+                if (deque.isEmpty()) {
+                    stringBuilder.append(ERROR).append(ENTER_LINE);
+                } else {
+                    stringBuilder.append(deque.peekLast()).append(ENTER_LINE);
+                }
+            }
+        }
+
+        System.out.print(stringBuilder);
+    }
+}
+
+/*
+문제
+정수를 저장하는 덱(Deque)를 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+
+명령은 총 여덟 가지이다.
+
+push_front X: 정수 X를 덱의 앞에 넣는다.
+push_back X: 정수 X를 덱의 뒤에 넣는다.
+pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+size: 덱에 들어있는 정수의 개수를 출력한다.
+empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+입력
+첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다. 둘째 줄부터 N개의 줄에는 명령이 하나씩 주어진다. 주어지는 정수는 1보다 크거나 같고, 100,000보다 작거나 같다. 문제에 나와있지 않은 명령이 주어지는 경우는 없다.
+
+출력
+출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
+
+예제 입력 1
+15
+push_back 1
+push_front 2
+front
+back
+size
+empty
+pop_front
+pop_back
+pop_front
+size
+empty
+pop_back
+push_front 3
+empty
+front
+예제 출력 1
+2
+1
+2
+0
+2
+1
+-1
+0
+1
+-1
+0
+3
+예제 입력 2
+22
+front
+back
+pop_front
+pop_back
+push_front 1
+front
+pop_back
+push_back 2
+back
+pop_front
+push_front 10
+push_front 333
+front
+back
+pop_back
+pop_back
+push_back 20
+push_back 1234
+front
+back
+pop_back
+pop_back
+예제 출력 2
+-1
+-1
+-1
+-1
+1
+1
+2
+2
+333
+10
+10
+333
+20
+1234
+1234
+20
+ */
